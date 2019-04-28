bd5259d92e17114a8b24802bae0e209eaefc3ebf
agusgs <agustin.garcia.smith@10pines.com>
2019-04-28 14:59:32 -0300

    agrego redux al proyecto

---
+++ src/store.js
@@ -0,0 +1,5 @@
+import { compose, createStore } from "redux";
+import { rootReducer } from "./rootReducer";
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+export const storeInstance = createStore(rootReducer, undefined, composeEnhancers());
