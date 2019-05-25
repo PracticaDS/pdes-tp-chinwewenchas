65d68af5e10cbb0eae279a1feda8161770e8399d
Alvaro <alvaropiorno100@gmail.com>
2019-05-25 15:13:47 -0300

    changes to crafter

--- src/components/machines/crafter/Crafter.js
+++ src/components/machines/crafter/Crafter.js
@@ -1,10 +1,12 @@
 import React from 'react'
-import { Machine } from '../machine/Machine'
 import PropTypes from 'prop-types'
+import './Crafter.css'
+import { connect } from 'react-redux'
+import { Machine } from '../machine/Machine'
 
-export const Crafter = ({ active, direction }) => {
+export const Crafter = ({ position, active, onClick, direction }) => {
   return (
-    <div className="crafter">
+    <div className="crafter" onClick={() => onClick(position)}>
       <Machine
         active={active}
         direction={direction}
@@ -17,5 +19,16 @@ export const Crafter = ({ active, direction }) => {
 
 Crafter.propTypes = {
   active: PropTypes.bool,
+  onClick: PropTypes.func,
+  position: PropTypes.object,
   direction: PropTypes.object
 }
+
+const mapDispatchToProps = dispatch => ({
+  onClick: position => dispatch(position => {})
+})
+const connector = connect(
+  undefined,
+  mapDispatchToProps
+)
+export default connector(Crafter)
