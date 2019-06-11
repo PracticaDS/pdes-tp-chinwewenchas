a8e35929d6676d7f77e6c4b80ff46842edca2927
Alvaro <alvaropiorno100@gmail.com>
2019-06-11 01:16:37 -0300

    more tests, click not working

--- e2e/cypress/integration/factory.spec.js
+++ e2e/cypress/integration/factory.spec.js
@@ -1,9 +1,5 @@
 describe('Factory', () => {
-  beforeEach('Borrar datos', () => {
-    // Antes de cada test remueve todos los datos
-    // cy.request("DELETE", "http://localhost:3000/api/todos")
-  })
-
+  
   describe('Una vez cargada la pagina', () => {
     beforeEach('Cargar la pagina', () => {
       cy.visit('http://localhost:3000')
@@ -20,10 +16,55 @@ describe('Factory', () => {
         .should('have.attr', 'value', 'Chinwewencha')
 
       cy.get('.sign_in_form_button')
-        .wait(3000)
-        .click({ force: true })
-      cy.wait(3000)
+        .invoke('width').should('be.greaterThan', 0)
+      cy.get('.sign_in_form_button').wait(1000).should('be.visible')
+        .click({force: true})
 
+      })
+
+      describe('Al completar el usuario y tocar Sign in', () => {
+
+
+      it('debe mostrar los elementos de saludo al usuario logueado y elementos de creación de fábrico', () => {
+      // Sign in
+      cy.get('.sign_in_container')
+      cy.get('.sign_in_form')
+      cy.get('.sign_in_form_input')
+        .then(function ($input) {
+          $input[0].setAttribute('value', 'Chinwewencha')
+        })
+        .should('have.attr', 'value', 'Chinwewencha')
+
+      cy.get('.sign_in_form_button')
+        .invoke('width').should('be.greaterThan', 0)
+      cy.get('.sign_in_form_button').wait(1000).should('be.visible')
+        .click({force: true})
+/*
+      cy.get('.factory_selector_container')
+      cy.get('.factory_selector_form')
+        .find('h1')
+        .contains('Whale cum Chinwewencha! ')
+      cy.get('.factory_selector_form_input')
+*/
+      
+    })
+    })
+      describe('Al completar los datos de creación de fábrica y tocar Create Factory', () => {
+      it('debe mostrar los elementos de la factory, toolbox, ganancias', () => {
+      // Sign in
+      cy.get('.sign_in_container')
+      cy.get('.sign_in_form')
+      cy.get('.sign_in_form_input')
+        .then(function ($input) {
+          $input[0].setAttribute('value', 'Chinwewencha')
+        })
+        .should('have.attr', 'value', 'Chinwewencha')
+
+      cy.get('.sign_in_form_button')
+        .invoke('width').should('be.greaterThan', 0)
+      cy.get('.sign_in_form_button').wait(1000).should('be.visible')
+        .click({force: true})
+/*
       cy.get('.factory_selector_container')
       cy.get('.factory_selector_form')
         .find('h1')
@@ -40,12 +81,8 @@ describe('Factory', () => {
         })
         .should('have.attr', 'value', 3)
 
-      cy.get(".factory_selector_form_button").click("Sign In")
-
-      cy.wait('.incomes-container')
-      .its('status')
-      .should('eq', 200)
-
+      cy.get(".factory_selector_form_button").click({force: true})
+*/
       // Ganancias
       // cy.get(".incomes-container")
       // cy.get(".incomes")
@@ -77,5 +114,6 @@ describe('Factory', () => {
       // cy.get(".factory_item")
       // cy.get(".factory_item")
     })
+  })    
   })
 })
